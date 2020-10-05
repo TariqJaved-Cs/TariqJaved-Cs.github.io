3ad531103f0b08458f4d1bdec0fd2e0b5cc5e510
Tariq Javed <55314564+TariqJaved-Cs@users.noreply.github.com>
2020-10-05 15:26:42 +0500

    Updates

---
+++ precache-manifest.7420c83d3cafa91ae1a289dbfcf5e455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7861a9ca37c4eff6631e518ea00fd3",
+    "url": "/TariqJaved-Cs.github.io/index.html"
+  },
+  {
+    "revision": "75832d7f63630646ce16",
+    "url": "/TariqJaved-Cs.github.io/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "30fccb3697acd7704da1",
+    "url": "/TariqJaved-Cs.github.io/static/css/main.8ced2fd3.chunk.css"
+  },
+  {
+    "revision": "75832d7f63630646ce16",
+    "url": "/TariqJaved-Cs.github.io/static/js/2.ddcc5d34.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/TariqJaved-Cs.github.io/static/js/2.ddcc5d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fccb3697acd7704da1",
+    "url": "/TariqJaved-Cs.github.io/static/js/main.c322f299.chunk.js"
+  },
+  {
+    "revision": "e22b010cb903faa35094",
+    "url": "/TariqJaved-Cs.github.io/static/js/runtime-main.1824221d.js"
+  },
+  {
+    "revision": "4cb252f1dd15a27c92f7235fb5765e87",
+    "url": "/TariqJaved-Cs.github.io/static/media/download.4cb252f1.webp"
+  }
+]);
